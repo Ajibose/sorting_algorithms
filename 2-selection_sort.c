11c24b412b61c1683757865a855564d3a831f269
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-22 22:54:55 +0000

    Task2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,20 +9,39 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i;
+	unsigned int i, j, min;
 
 	if (size < 2 || array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		min = i
-		for (j = i + 1; i < size; j++)
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
 		}
 		if (i != min)
+		{
 			swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
 	}
+}
+
+/**
+ * swap - swap two variables
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
